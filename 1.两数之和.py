726d427195786c320257dfd24e1dbab58e3a0fd5
qixin <1143541029@qq.com>
2024-11-22 00:08:40 +0800

    update

--- 1.两数之和.py
+++ 1.两数之和.py
@@ -8,9 +8,18 @@ from typing import List
 # @lc code=start
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        n = len(nums)
-        for i in range(n):
-            for j in range(i+1, n):
-                if nums[i] + nums[j] == target:
-                    return [i,j]
+        # 双指针
+        # n = len(nums)
+        # for i in range(n):
+        #     for j in range(i+1, n):
+        #         if nums[i] + nums[j] == target:
+        #             return [i,j]
+
+        index_map = {}
+        for index, value in enumerate(nums):
+            other_value = target - value
+            if (other_value in index_map.keys()):
+                return [index, index_map[other_value]]
+            index_map[value]=index
+        
 # @lc code=end
